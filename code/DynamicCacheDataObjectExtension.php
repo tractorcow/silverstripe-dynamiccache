f70e826951818f5d4224d3baad49ce14c2adcf0b
Jake B <jake@symbiote.com.au>
2017-07-24 12:45:35 +1000

    feat(DynamicCacheDataObjectExtension): Update extension so the cache is only cleared on publishing if the DataObject is Versioned.

--- code/DynamicCacheDataObjectExtension.php
+++ code/DynamicCacheDataObjectExtension.php
@@ -16,9 +16,21 @@ class DynamicCacheDataObjectExtension extends DataExtension
      */
     public function onAfterWrite()
     {
-        if (DynamicCache::config()->cacheClearOnWrite) {
-            DynamicCache::inst()->clear();
+        if (!DynamicCache::config()->cacheClearOnWrite) {
+            return;
         }
+
+        // Do not clear cache if object is Versioned. Only clear
+        // when a user publishes.
+        //
+        // DynamicCacheControllerExtension already opts out of caching if 
+        // on ?stage=Stage so this behaviour makes sense.
+        //
+        if ($this->hasLiveStage()) {
+            return;
+        }
+
+        DynamicCache::inst()->clear();
     }
 
     /**
@@ -28,8 +40,53 @@ class DynamicCacheDataObjectExtension extends DataExtension
      */
     public function onBeforeDelete()
     {
-        if (DynamicCache::config()->cacheClearOnWrite) {
-            DynamicCache::inst()->clear();
+        if (!DynamicCache::config()->cacheClearOnWrite) {
+            return;
         }
+        DynamicCache::inst()->clear();
+    }
+
+    /**
+     * Support Versioned::publish()
+     * - Use case: SheaDawson Blocks module support
+     */
+    public function onBeforeVersionedPublish() {
+        if (!DynamicCache::config()->cacheClearOnWrite) {
+            return;
+        }
+        DynamicCache::inst()->clear();
+    }
+
+    /**
+     * Support SiteTree::doPublish()
+     */
+    public function onAfterPublish() {
+        if (!DynamicCache::config()->cacheClearOnWrite) {
+            return;
+        }
+        DynamicCache::inst()->clear();
+    }
+
+    /**
+     * Support HeyDay's VersionedDataObject extension
+     * - Use case: DNADesign Elemental support
+     */
+    public function onAfterVersionedPublish() {
+        if (!DynamicCache::config()->cacheClearOnWrite) {
+            return;
+        }
+        DynamicCache::inst()->clear();
+    }
+
+    protected function hasLiveStage() {
+        $class = $this->owner->class;
+        // NOTE: Using has_extension over hasExtension as the former
+        //       takes subclasses into account.
+        $hasVersioned = $class::has_extension('Versioned');
+        if (!$hasVersioned) {
+            return false;
+        }
+        $stages = $this->owner->getVersionedStages();
+        return $stages && in_array('Live', $stages);
     }
 }
